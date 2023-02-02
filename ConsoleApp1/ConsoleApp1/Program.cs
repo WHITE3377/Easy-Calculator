fbb522d860647038c13dbbe0553da43cb64a3eab
WHITE3377 <112165280+WHITE3377@users.noreply.github.com>
2023-02-02 16:51:15 +0200

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,40 +1,54 @@
-﻿double FirstNum, SecondNum;
-
-string action;
-
-Console.WriteLine("Enter first number");
-
-FirstNum = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Enter second number");
-
-SecondNum = double.Parse(Console.ReadLine());
-
-Console.WriteLine("select operation: '+' '-' '/' '*' ");
-action = Console.ReadLine();
-
-switch (action)
+﻿while (true)
 {
-    case "+":
-        Console.WriteLine(FirstNum + SecondNum);
-        break;
-    case "-":
-        Console.WriteLine(FirstNum - SecondNum);
-        break;
-    case "*":
-        Console.WriteLine(FirstNum * SecondNum);
-        break;
-    case "/":
-        if (SecondNum == 0)
-        {
-            Console.WriteLine(0);
-        }
-        else
-        {
-            Console.WriteLine(FirstNum / SecondNum);
-        } 
-        break;
-    default:
-        Console.WriteLine("Error! unknown action!");
-        break;
+    Console.Clear();
+
+    double FirstNum, SecondNum;
+    string action;
+
+    try
+    {
+        Console.WriteLine("Enter first number");
+        FirstNum = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter second number");
+        SecondNum = double.Parse(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Error.");
+        Console.WriteLine("Press Enter to Restart Calculator");
+        Console.ReadLine();
+        continue;
+    }
+
+    Console.WriteLine("select operation: '+' '-' '/' '*' ");
+    action = Console.ReadLine();
+
+    switch (action)
+    {
+        case "+":
+            Console.WriteLine(FirstNum + SecondNum);
+            break;
+        case "-":
+            Console.WriteLine(FirstNum - SecondNum);
+            break;
+        case "*":
+            Console.WriteLine(FirstNum * SecondNum);
+            break;
+        case "/":
+            if (SecondNum == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(FirstNum / SecondNum);
+            }
+            break;
+        default:
+            Console.WriteLine("Error! unknown action!");
+            break;
+    }
+    Console.WriteLine("Press Enter to Restart Calculator");
+    Console.ReadLine();
 }
