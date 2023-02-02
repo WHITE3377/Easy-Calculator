7fd2998c139c5ae29e28d85d370cdb01d93d2110
WHITE3377 <112165280+WHITE3377@users.noreply.github.com>
2023-02-02 16:15:10 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿double FirstNum, SecondNum;
+
+string action;
+
+Console.WriteLine("Введiть перше число");
+
+FirstNum = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введiть друге число");
+
+SecondNum = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Виберiть операцію: '+' '-' '/' '*' ");
+action = Console.ReadLine();
+
+switch (action)
+{
+    case "+":
+        Console.WriteLine(FirstNum + SecondNum);
+        break;
+    case "-":
+        Console.WriteLine(FirstNum - SecondNum);
+        break;
+    case "*":
+        Console.WriteLine(FirstNum * SecondNum);
+        break;
+    case "/":
+        if (SecondNum == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            Console.WriteLine(FirstNum / SecondNum);
+        } 
+        break;
+    default:
+        Console.WriteLine("Ви написали не той знак");
+        break;
+
+}
